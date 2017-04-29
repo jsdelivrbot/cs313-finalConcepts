2a3a9315492490707dce5aef4c0cb7d3aab62f11
zachHulse <hul12014@byui.edu>
2017-04-29 03:03:14 -0600

    completed the prove assignment, updating html and css files and adding js file

---
+++ web/hello.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#history").click(function(){
+        $("h2").text("History");
+        $("#info").text("Some of my personal percussion history");
+        $("#one").text("Younger Years");
+        $("#two").text("Music Major");
+        $("#three").text("Now");
+        $("#img1").attr("src", "http://im2.woodbrass.com/images/woodbrass/GLOCKENSPIEL+BERGERAULT+GV.JPG");
+        $("#img2").attr("src", "https://s-media-cache-ak0.pinimg.com/564x/61/b8/01/61b8019c4285cfd2899359b4febad718.jpg");
+        $("#img3").attr("src", "https://blog.andertons.co.uk/wp-content/uploads/2013/02/SNM529XFB-RLE.jpg");
+        $("#p1").text("I began playing percussion in sixth grade band. I played all through school and one year I was the only percussionist in our high school band. I also had a couple bands out of my parents and other friends garages.");
+        $("#p2").text("When I got to BYU-Idaho I spent four semesters as a Percussion Performance major and learned a lot from my peers and great facaulty. I also entertained the idea of being a music education major, but throught this time struggled to keep up with the demands of learning rudimental snare drum, timpani, marimba, and keep up on my drum set playing.")    
+        $("#p3").text("The time I spent as a music major taught me that I may not want or be able to make playing music my career right now, but I gained a deeper love and appreciation for music as an art form. I have also been set on a journey of lifelong growth and development in music. I also believe that the lessons I learned apply to other areas of life and hope I will be able to connect my love of music with my budding software skills.");
+    });
+});
+
+$(document).ready(function(){
+    $("#drummers").click(function(){
+        $("h2").text("Drummers");
+        $("#info").text("Some drummers I like");
+        $("#one").text("JoJo Mayer");
+        $("#two").text("Benny Greb");
+        $("#three").text("Matt Garstka");
+        $("#img1").attr("src", "http://vicfirth.com/wp-content/uploads/2015/10/jojo_mayer.jpg");
+        $("#img2").attr("src", "https://a3-images.myspacecdn.com/images04/10/6dda4eaaa38640a0afb7b5395582cd1c/300x300.jpg");
+        $("#img3").attr("src", "http://www.sticks.de/wp-content/uploads/2016/10/Matt-Garska.jpg");
+        $("#p1").text("Jojo Mayer is known for bringing together jazz, drum and bass, jungle, and other influences. He is also famous for his ability to play the rhythms of programmed jungle drum 'n' bass music on acoustic drums, terming this technique reverse engineering, a phrase borrowed from computing. He executes this approach using various methods such as the heel-toe bass drum technique, the Moeller stroke, the Valving technique, and the pull-out or push-pull accent which takes the beat on the rebound. Using these techniques, he can play beats at astonishing speeds. A few of his main influences are Tony Williams, Jack DeJohnette and Buddy Rich. -Wikipedia");
+        $("#p2").text("Benny Greb (born June 13, 1980 in Augsburg, Germany) is a prolific German drummer and clinician. He started playing the drums at age six and began taking lessons at age twelve. He plays a large variety of music and can be seen playing rock with Stoppok (de) and The Ron Spielman Trio, jazz with The Benny Greb Brass Band and Sabri Tulug Tirpan, funk with Jerobeam, reggae with Bobby McFerrin, fusion with the NDR Big Band on their Frank Zappa Project, with 3erGezimmeR and Wayne Krantz, and acoustic punk with Strom & Wasser. He has also performed at the Modern Drummer Festival 2010 and at many clinics and drum festivals around the world. - Wikipedia")    
+        $("#p3").text("Garstka, son of professional guitar player Greg Garstka, was born in Hopewell, Virginia, and raised in Westfield, Massachusetts. He started playing drums at the age of 8. At the age of 12 he started gigging with his father, playing rock, blues and reggae music. At 14 Garstka met Jo Sallins, who introduced him to fusion, funk, Latin and jazz drumming. At the age of 14 Matt appeared in Sallins DVD Mr. Cool. At the age of 16 Garstka began taking lessons with Bob Gullotti. In 2006 he released an EP with punk band Backstab and in 2007 released his first solo album Heavy Volume, featuring Joel Stroetzel from Killswitch Engage, Chis Regan from FNB and Jo Sallins. After finishing high school, Garstka attended Berklee College of Music. While studying at Berklee, Matt toured with African hip hop group Gokh-Bi System. In 2011 Garstka graduated and moved to Los Angeles. In the same year he was featured in GospelChops.com DVD Shed Sessionz Vol. 3. Soon after moving to Los Angeles Garstka met Tosin Abasi and joined his band Animals as Leaders following the departure of Navene Koperweis. With Animals as Leaders, Garstka released The Joy of Motion in 2014. In March 2015 Garstka was featured as Modern Drummer magazine cover artist - Wikipedia");
+    });
+});
+
+$(document).ready(function(){
+    $("#stuff").click(function(){
+        $("h2").text("My Stuff");
+        $("#info").text("Some hand-hammered cymbals that I am the proud owner of.");
+        $("#one").text("19in HHX Sabian OMNI");
+        $("#two").text("22in Meinl Byzance Spectrum ride");
+        $("#three").text("14in Meinl Byzance Dark hats");
+        $("#img1").attr("src", "http://www.justdrums.com/wp-content/uploads/2014/08/122omx-22-inch-hhx-omni_full.png");
+        $("#img2").attr("src", "http://static.keymusic.com/products/114794/XL/meinl-b22sr-byzance-dark-spectrum-ride-22.jpg");
+        $("#img3").attr("src", "https://www.thomann.de/pics/bdb/225840/11841110_800.jpg");
+        $("#p1").text("This was the first hand hammered cymbal that I ever bought. It is a very unique cymbal because of the unlathed middle portion. You can get a wider variety of tones because of this design. JoJo Mayer helped come up with this cymbal.");
+        $("#p2").text("Jost Nickel, another drummer I like, played the very cymbal I bought from MyCymbal.com. This cymbal has a spiral lathing that gives this cymbal a unique look I really like. I also love the way it sounds and would feel comfortable taking it to most any playing situation.")    
+        $("#p3").text("These hi-hats have a cool sound that I think is kind of trashy. They are a bit heavier than I expected when I was buying them, but think the are a great pair of cymbals that I am glad to have as part of my sound.");
+    });
+});
